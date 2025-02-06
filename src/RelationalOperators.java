a527f7989aaa6af7060994e4879335891f44016f
its-kios09 <legacyitsolution@gmail.com>
2025-02-06 17:32:42 +0300

    (feat) added the relational  operators program

---
+++ src/RelationalOperators.java
@@ -0,0 +1,10 @@
+public class RelationalOperators {
+//    Relational operators are operators used for comparison which require two operands
+    public static void main(String[] args){
+        int x = 10;
+        int y = 5;
+
+        System.out.println(x > y);
+        System.out.println(x < y);
+    }
+}
--- src/ShiftOperator.java
+++ src/ShiftOperator.java
@@ -1,2 +1,8 @@
 public class ShiftOperator {
+//    Shift operators are used to shift the binary bits for a specific number of times on either left or right based on the users requests and the results are in a new decimal value.
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x << 2);
+        System.out.println(x >> 2);
+    }
 }
