4fb67cc441d5136834c6b4701595f8a9f085800a
its-kios09 <legacyitsolution@gmail.com>
2025-02-06 17:37:43 +0300

    (feat) ternary operation program

---
+++ src/TernaryOperator.java
@@ -0,0 +1,10 @@
+public class TernaryOperator {
+//    Ternary operator requires two operands which reduces the length of the code and work load processor
+
+    public static void main(String[] args){
+        int x = 10;
+        int y = 5;
+        int z = x > y ? x : y;
+        System.out.println(z);
+    }
+}
