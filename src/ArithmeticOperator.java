a47bdf14452f0752dd40f486f4e6412997b54eeb
its-kios09 <legacyitsolution@gmail.com>
2025-02-06 17:24:34 +0300

    (feat) added the arithmetic operators program

--- src/ArithmeticOperator.java
+++ src/ArithmeticOperator.java
@@ -1,3 +1,13 @@
 public class ArithmeticOperator {
+//    Arithmetic operators performs basic additions, subtractions, division, multiplication, division and other operations
+    public static void main(String[] args){
+        int x = 10;
+        int y = 5;
+        System.out.println( x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x/y);
+        System.out.println(y % x);
 
+    }
 }
